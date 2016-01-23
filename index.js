a1c9d98b40dcaa8aea54743258287fc6f24eaab2
Jalsemgeest <alsemgeester@gmail.com>
2016-01-23 14:16:17 -0500

    Added Clarifai API

--- index.js
+++ index.js
@@ -2,9 +2,23 @@
 var clarifai_access_token_request = "https://7XcRYTSUu-X4tt2-3ojUsaepRtfPpHgk5VtvQn0n:8KU5maZusTMVL3PL7ZbXw_VOuD42shAhJM-1RrAB@api.clarifai.com/v1/token/"
 var clarifai_access_token_header = "grant_type=client_credentials"
 
+var clarifai_access_token = null;
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var stylus = require('stylus');
+var Clarifai = require('clarifai');
+
+clarifai_client = new Clarifai({
+  id: "7XcRYTSUu-X4tt2-3ojUsaepRtfPpHgk5VtvQn0n",
+  secret: "8KU5maZusTMVL3PL7ZbXw_VOuD42shAhJM-1RrAB"
+});
+
+clarifai_client.getAccessToken(function(err, accessToken) {
+	if (!err) {
+	  clarifai_access_token = accessToken;
+	}
+});
 
 var app = express();
 
@@ -36,5 +50,17 @@ var port = process.env.PORT || 3000;
 
 server.listen(port, function() {
 
+	// var url = "https://upload.wikimedia.org/wikipedia/commons/0/00/Wgretz_edit2.jpg"
+
     console.log("Server is listening on port " + port.toString());
+
+ //    clarifai_client.tagFromUrls('image', url, function(err, results) {
+	//   console.log(results);
+	// }, null);
 });
+
+app.post('/findPhotoTags', function(req, res) {
+	
+});
+
+
