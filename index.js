73410ed897be02a98f4f9ab532978cbe28322c52
zharley18 <zackharley@gmail.com>
2016-01-23 14:23:34 -0500

    Added dom-parser to DOM_Parser branch

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var stylus = require('stylus');
 var Clarifai = require('clarifai');
+var express = require('express')
+var bodyParser = require('body-parser')
+ 
+var app = express()
+ 
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json 
+app.use(bodyParser.json())
+ 
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
 
 clarifai_client = new Clarifai({
   id: "7XcRYTSUu-X4tt2-3ojUsaepRtfPpHgk5VtvQn0n",
