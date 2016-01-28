59a043d32ab4b3aa151ad8feed8934c2e03adf60
Jalsemgeest <alsemgeester@gmail.com>
2016-01-27 23:00:21 -0500

    Heroku needs the constants.js file.

---
+++ constants.js
@@ -0,0 +1,6 @@
+function Constants() {
+	this.clarifai_access_token_id = "7XcRYTSUu-X4tt2-3ojUsaepRtfPpHgk5VtvQn0n";
+	this.clarifai_access_token_secret = "8KU5maZusTMVL3PL7ZbXw_VOuD42shAhJM-1RrAB";
+}
+
+module.exports = Constants;
